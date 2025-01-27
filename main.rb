427f9d3217b6dc0d68bb2f60cbc1c5b724b77ce1
deniedwin <24278798+deniedwin@users.noreply.github.com>
2025-01-27 17:22:20 +0100

    Used the .select method for the dictionary array By doing so i can match the all the words in Dictionary with word_instances

--- main.rb
+++ main.rb
@@ -1,7 +1,14 @@
-def sub_strings(word, dict)
-  word_index = 0;
-  while word_index != word.length
-    puts word[word_index..word.length]
+# Implement a method #substrings that takes a word as the first argument 
+# and then an array of valid substrings (your dictionary) as the second argument. 
+# It should return a hash listing each substring (case insensitive) 
+# that was found in the original string and how many times it was found.
+
+def substrings(my_word, dict)
+  word_index = 0
+  word_instances = ""
+  while word_index != my_word.length
+    word_instances =  my_word[word_index..my_word.length]
+    puts dict.select {|word| word == word_instances}
     word_index += 1
   end
 end
@@ -9,4 +16,4 @@ end
 dictionary = ["below","down","go","going","horn","how","howdy",
 "it","i","low","own","part","partner","sit"]
 
-sub_strings("below", 5)
+substrings("below", dictionary)
